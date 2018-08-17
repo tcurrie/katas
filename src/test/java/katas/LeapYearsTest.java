38521a7935ba4f143350e9612244ec6a4872bd09
Currie, Todd <Todd.Currie@cdk.com>
2018-08-17 09:50:33 -0700

    Leap years kata.

---
+++ src/test/java/katas/LeapYearsTest.java
@@ -0,0 +1,44 @@
+package katas;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+
+import org.junit.Test;
+
+public class LeapYearsTest {
+
+    @Test
+    public void yearsDivisibleBy4AreLeapYears() {
+        validate(true, 4, 8, 12, 16, 96);
+    }
+
+    @Test
+    public void yearsNotDivisibleBy4AreNotLeapYears() {
+        validate(false, 1, 2, 3, 5, 6, 7, 9, 95, 97);
+    }
+
+    @Test
+    public void yearsDivisibleBy100AreNotLeapYears() {
+        validate(false, 100, 200, 300, 900);
+    }
+
+    @Test
+    public void yearsDivisibleBy400AreLeapYears() {
+        validate(true, 400, 800, 1200, 3600);
+    }
+
+    @Test
+    public void yearsDivisibleBy4000AreNotLeapYears() {
+        validate(false, 4000, 8000, 12000, 36000);
+    }
+
+    private void validate(final boolean expected, final int... years) {
+        for (final int year : years) {
+            assertThat(isLeapYear(year), is(expected));
+        }
+    }
+
+    private boolean isLeapYear(final int year) {
+        return year % 400 == 0 && year % 4000 != 0 || year % 4 == 0 && year % 100 != 0;
+    }
+}
